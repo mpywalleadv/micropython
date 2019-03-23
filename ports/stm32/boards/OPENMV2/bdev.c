bb29bce1d3053027365caaaa27403fc013a4e28c
xiabizha <robin1002@163.com>
2019-03-23 22:04:25 +0800

    enable flash cache, fix the issue of reset in case of modify file when run the script

--- ports/stm32/boards/OPENMV2/bdev.c
+++ ports/stm32/boards/OPENMV2/bdev.c
@@ -11,15 +11,15 @@ const mp_soft_spi_obj_t soft_spi_bus = {
     .miso = MICROPY_HW_SPIFLASH_MISO,
 };
 
-//STATIC mp_spiflash_cache_t spi_bdev_cache;
+STATIC mp_spiflash_cache_t spi_bdev_cache;
 
 const mp_spiflash_config_t spiflash_config = {
     .bus_kind = MP_SPIFLASH_BUS_SPI,
     .bus.u_spi.cs = MICROPY_HW_SPIFLASH_CS,
     .bus.u_spi.data = (void*)&soft_spi_bus,
     .bus.u_spi.proto = &mp_soft_spi_proto,
-	.cache = NULL,
-//    .cache = &spi_bdev_cache,
+//	.cache = NULL,
+    .cache = &spi_bdev_cache,
 };
 
 spi_bdev_t spi_bdev;
